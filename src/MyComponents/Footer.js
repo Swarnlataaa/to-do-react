839e375dcfcd5f5f13cdc08d93f9f668af483cfb
Swarnlata <82523453+Swarnlataaa@users.noreply.github.com>
2023-02-21 19:00:54 +0530

    initial commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-dark text-light py-3">
+      <p className="text-center">Copyright 2022 &copy; myworklist.com</p>
+    </footer>
+  );
+};
